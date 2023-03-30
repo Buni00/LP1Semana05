cd488ccb3cea68b77a006bb0820f8897bc3dbc6f
Buni00 <bruno_hrf@outlook.com>
2023-03-31 00:44:21 +0100

    Ask for number of dice

---
+++ RandomDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Número de dados a lançar: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
