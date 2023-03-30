b7f301c951337beac5b942772be22bd6bb5e7207
Buni00 <bruno_hrf@outlook.com>
2023-03-31 00:35:16 +0100

    Add time conversion to seconds

---
+++ Tempo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(500);
+
+            crono2.Start();
+            Thread.Sleep(300);
+
+            crono1.Stop();
+            crono2.Stop();
+
+           float time1sec = crono1.ElapsedMilliseconds / 100f;
+           float time2sec = crono2.ElapsedMilliseconds / 100f;
+           
+
+           Console.WriteLine(time1sec);
+           Console.WriteLine(time2sec);
+
+
+        }
+    }
+}
