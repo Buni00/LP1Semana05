debe9a08ece5087310eba43e1ff7c4dce69d6f32
Buni00 <bruno_hrf@outlook.com>
2023-03-31 00:52:08 +0100

    Add dice rolls and counter

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -8,7 +8,24 @@ namespace RandomDice
         {
             Console.Write("Número de dados a lançar: ");
             int n = Convert.ToInt32(Console.ReadLine());
-            
+
+            int counter = 0;
+            Random rnd = new Random();
+
+
+            for (int i = 1; i<=n; i++)
+            {
+                
+                
+                int die = rnd.Next(1,6); 
+                
+                counter += die;
+                Console.WriteLine($"Roll {i}: {die} ");
+                
+
+                
+            }
+            Console.WriteLine(counter);
         }
     }
 }
