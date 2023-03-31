85fed5d77b280bc0ccaf3dc4dee23fccfc3b36b3
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-31 13:33:07 +0100

    Add string conversion to lower case and sorting

---
+++ LowerUnion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LowerUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array.Sort(args); 
+
+            for (int i = 0; i < args.Length; i++)
+            {   
+                args[i] = args[i].ToLower();
+                Console.WriteLine(args[i]);
+            }
+
+               
+
+            
+        }
+    }
+}
